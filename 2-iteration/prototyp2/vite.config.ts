30210cf8e5f779cd1ecfbdc2307742fde927a495
orciument <orciument@gmail.com>
2025-08-16 12:57:08 +0200

    add basicSSL to be able to connect from mobile via local network

--- 2-iteration/prototyp2/vite.config.ts
+++ 2-iteration/prototyp2/vite.config.ts
@@ -2,13 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import * as path from "node:path";
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), basicSsl()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    cors: false,
+    host: true,
+  }
 })
