d31e9dae34d9cbc85c33f15246f663cecd6f8ac6
orciument <orciument@gmail.com>
2025-08-19 15:11:11 +0200

    add popup sheet for details

--- 2-iteration/prototyp2/src/scannerScreen/ProductScanner.tsx
+++ 2-iteration/prototyp2/src/scannerScreen/ProductScanner.tsx
@@ -11,6 +11,8 @@ import DiamondAlertIcon from "@/assets/diamond_alert.tsx";
 import DiamondCheckIcon from "@/assets/diamond_check.tsx";
 import AlertIconBare from "@/assets/explamation_mark.tsx";
 import {Allergen} from "@/common/Allergens.ts";
+import {Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger} from "@/components/ui/sheet";
+import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@/components/ui/carousel.tsx";
 
 export default function ProductScanner() {
   const videoDevices = useVideoDevices();
@@ -73,17 +75,47 @@ function PersonResult({warning}: { warning: FoodWarningReturn }) {
 function WarningResults({warnings}: { warnings: FoodWarningReturn[] }) {
   /*TODO this has to be clickable into a popout view because the cause lists could be pretty long*/
   const hasAWarning = warnings.map(value => value.has_warning).reduce((a, b) => a || b);
-  return <div className="result productScannerCard">
-    <div className="headline">
-      {hasAWarning ? <DiamondAlertIcon className="warningIcon"/> :
-        <DiamondCheckIcon className="okIcon"/>}
-      <p>Warning</p>
-    </div>
-    <div className="personList">
-      {warnings.map((value, i) => <>
-        <PersonResult key={i} warning={value}/>
-        <div className="verticalSeparator"/>
-      </>)}
-    </div>
-  </div>
+  return <Sheet>
+    <SheetTrigger>
+      <div className="result productScannerCard">
+        <div className="headline">
+          {hasAWarning ? <DiamondAlertIcon className="warningIcon"/> :
+            <DiamondCheckIcon className="okIcon"/>}
+          <p>Warning</p>
+        </div>
+        <div className="personList">
+          {warnings.map((value, i) => <>
+            <PersonResult key={i} warning={value}/>
+            <div className="verticalSeparator"/>
+          </>)}
+        </div>
+      </div>
+    </SheetTrigger>
+    <SheetContent side="bottom" style={{height: "100%", backgroundColor: "white"}}>
+      <SheetHeader>
+        <SheetTitle className="sheetTitle">
+          Warning results
+        </SheetTitle>
+      </SheetHeader>
+      <Carousel orientation="horizontal" opts={{loop: true}}>
+        <CarouselContent>
+          {warnings.map((value) =>
+            <CarouselItem className="carouselPerson">
+              <h2>{value.person_name}</h2>
+              <div className="resultConent">
+                <h3>Allergens:</h3>
+                <div>
+                  {value.matching_allergens.map(v => <div>{Allergen[v]}</div>)}
+                </div>
+                <h3>Incompatible ingredients:</h3>
+                <div>
+                  {value.matching_ingredients.map(v => <div>{v}</div>)}
+                </div>
+              </div>
+            </CarouselItem>
+          )}
+        </CarouselContent>
+      </Carousel>
+    </SheetContent>
+  </Sheet>
 }
