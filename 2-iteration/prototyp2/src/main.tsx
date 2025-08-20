ce6fde6455cfd28f5488ed3263a72bf930e5a8b2
orciument <orciument@gmail.com>
2025-08-21 00:04:53 +0200

    remove testing ui

--- 2-iteration/prototyp2/src/main.tsx
+++ 2-iteration/prototyp2/src/main.tsx
@@ -5,7 +5,6 @@ import HomeScreen from "@/homescreen/HomeScreen.tsx"
 import {createBrowserRouter, createRoutesFromChildren, Route, RouterProvider} from "react-router-dom";
 import AllergenSelectorTest from "@/settingsEditor/allergenSelector/AllergenSelectorTest.tsx";
 import ProfileManager from "@/settingsEditor/profile_manager/ProfileManager.jsx";
-import BarcodeScannerTest from "@/barcodeScanner/BarcodeScannerTest.tsx";
 import ProductScanner from "@/scannerScreen/ProductScanner.tsx";
 import ImageBlutTest from "@/imageContainer/ImageBlutTest.tsx";
 
@@ -16,7 +15,6 @@ const router = createBrowserRouter(
             <Route path="/productScanner" element={<ProductScanner/>}/>
             {/*Temp for testing \/*/}
             <Route path="/allergenSelectorTest" element={<AllergenSelectorTest/>}/>
-            <Route path="/barCodeScannerTest" element={<BarcodeScannerTest/>}/>
             <Route path="/imageBlurTest" element={<ImageBlutTest/>}/>
             <Route path="/profile_manager" element={<ProfileManager/>}/>
         </>
