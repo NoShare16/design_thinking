cf28e134db7985ed9d734fa4083bb938528d7a7c
orciument <orciument@gmail.com>
2025-08-22 22:04:03 +0200

    fix incorrect hasWarning calculation

--- 2-iteration/prototyp2/src/common/hooks/useFoodWarnings.ts
+++ 2-iteration/prototyp2/src/common/hooks/useFoodWarnings.ts
@@ -18,7 +18,7 @@ export function useFoodWarnings(product: ProductInfo | undefined) {
           person_name: p.name,
           matching_allergens: d.matchedAllergens,
           matching_ingredients: d.matchedIngredients,
-          has_warning: d.matchedIngredients.length + d.matchedIngredients.length > 0
+          has_warning: d.matchedAllergens.length + d.matchedIngredients.length > 0
         }
       });
       setWarnings(map);
